05175542f7ba7865a5a247fccb6df79a9a477322
kazuki0824 <kazukuro0824@gmail.com>
2022-06-27 22:46:23 +0900

    Unaligned bytes de/encryption

--- cbc/src/lib.rs
+++ cbc/src/lib.rs
@@ -92,22 +92,72 @@
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
     html_root_url = "https://docs.rs/cbc/0.1.2"
 )]
-#![forbid(unsafe_code)]
+#![deny(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![warn(missing_docs, rust_2018_idioms)]
 
 mod decrypt;
 mod encrypt;
+mod unaligned_bytes;
+mod unaligned_bytes_mut;
 
+pub use crate::unaligned_bytes_mut::{UnalignedBytesDecryptMut, UnalignedBytesEncryptMut};
 pub use cipher;
+use cipher::{
+    Block, BlockCipher, BlockDecrypt, BlockDecryptMut, BlockEncrypt, BlockEncryptMut, BlockSizeUser,
+};
 pub use decrypt::Decryptor;
 pub use encrypt::Encryptor;
 
 use cipher::generic_array::{ArrayLength, GenericArray};
+use cipher::inout::InOutBuf;
 
 #[inline(always)]
 fn xor<N: ArrayLength<u8>>(out: &mut GenericArray<u8, N>, buf: &GenericArray<u8, N>) {
     for (a, b) in out.iter_mut().zip(buf) {
         *a ^= *b;
+    }
+}
+
+/// If unaligned tail procesing failed, this struct should be returned.
+#[derive(Debug)]
+pub struct TailError;
+
+impl<C: BlockCipher + BlockDecryptMut + BlockEncrypt + BlockSizeUser> UnalignedBytesDecryptMut
+    for Decryptor<C>
+{
+    fn proc_tail(
+        &self,
+        blocks: &mut InOutBuf<'_, '_, Block<Self>>,
+        tail: &mut InOutBuf<'_, '_, u8>,
+    ) -> Result<(), TailError> {
+        match blocks.get_in().last() {
+            Some(last) => {
+                let mut last: Block<C> = last.clone();
+                self.cipher.encrypt_block(&mut last);
+                tail.xor_in2out(&last[0..tail.len()]);
+                Ok(())
+            }
+            None => Err(TailError {}),
+        }
+    }
+}
+impl<C: BlockCipher + BlockEncryptMut + BlockDecrypt + BlockSizeUser> UnalignedBytesEncryptMut
+    for Encryptor<C>
+{
+    fn proc_tail(
+        &self,
+        blocks: &mut InOutBuf<'_, '_, Block<Self>>,
+        tail: &mut InOutBuf<'_, '_, u8>,
+    ) -> Result<(), TailError> {
+        match blocks.get_in().last() {
+            Some(last) => {
+                let mut last: Block<C> = last.clone();
+                self.cipher.decrypt_block(&mut last);
+                tail.xor_in2out(&last[0..tail.len()]);
+                Ok(())
+            }
+            None => Err(TailError {}),
+        }
     }
 }
